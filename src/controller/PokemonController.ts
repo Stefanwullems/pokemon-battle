2ea44189856b9c2f21c0b69c3a8701d8fb958839
Vince Liem <vincepaul.liem@gmail.com>
2018-10-19 11:34:22 +0200

    Finished database backend sprint 1

---
+++ src/controller/PokemonController.ts
@@ -0,0 +1,34 @@
+import { Controller, Query } from "vesper";
+import { EntityManager } from "typeorm";
+import { Pokemon } from "../entity/Pokemon";
+
+@Controller()
+export class PokemonController {
+  constructor(private entityManager: EntityManager) {}
+
+  // serves "posts: [Post]" requests
+  @Query()
+  allPokemon() {
+    return this.entityManager.find(Pokemon);
+  }
+
+  // serves "post(id: Int): Post" requests
+  @Query()
+  pokemon({ id }) {
+    return this.entityManager.findOne(Pokemon, id);
+  }
+
+//   // serves "postSave(id: Int, title: String, text: String): Post" requests
+//   @Mutation()
+//   postSave(args) {
+//     const post = this.entityManager.create(Post, args);
+//     return this.entityManager.save(Post, post);
+//   }
+//
+//   // serves "postDelete(id: Int): Boolean" requests
+//   @Mutation()
+//   async postDelete({ id }) {
+//     await this.entityManager.remove(Post, { id: id });
+//     return true;
+//   }
+}
