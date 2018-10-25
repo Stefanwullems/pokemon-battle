b340453598296924fef82c8b47cb9086fa2788f5
Vince Liem <vincepaul.liem@gmail.com>
2018-10-25 12:20:25 +0200

    thingy

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,45 @@
-import { bootstrap } from "vesper";
-import * as IO from 'socket.io'
+import { bootstrap } from "vesper"
+import * as IO from "socket.io"
 
-import { PokemonController } from "./controller/PokemonController";
-import { Pokemon, Move, Stats, Type } from "./entity/Pokemon";
+import { PokemonController } from "./controller/PokemonController"
+import { Pokemon, Move, Stats, Type } from "./entity/Pokemon"
 
-const port: number = Number(process.env.PORT) || 4011;
+// import { Server } from "socket.io"
+
+const port: number = Number(process.env.PORT) || 4011
+
+// const app: VesperFramework = new VesperFramework({
+//   cors: true,
+//   port,
+//   controllers: [PokemonController],
+//   entities: [Pokemon, Move, Stats, Type],
+//   schemas: ["../**/*.graphql"]
+// });
+//
+// export const io: Server = IO(app.server);
+//
+// app.start()
+// // bootstrap(app.options)
+// .then(() => {
+//
+//   io.on('connect', socket => {
+//     const color = socket.request._query.color;
+//     console.log(`Trainer ${color} just connected`);
+//
+//     socket.on('disconnect', () => {
+//       console.log(`Trainer ${color} just disconnected`)
+//     })
+//   });
+//
+//   console.log(
+//     `Your app is up and running on http://localhost:${port}.
+//       You can use playground in development mode on http://localhost:${port}/playground`
+//   );
+// })
+// .catch(error => {
+//   console.error(error.stack ? error.stack : error);
+// });
+
 
 bootstrap({
   cors: true,
@@ -13,24 +48,60 @@ bootstrap({
   entities: [Pokemon, Move, Stats, Type],
   schemas: ["../**/*.graphql"]
 })
-  .then((framework) => {
+.then(({ server }) => {
 
-    const io = IO(framework.server);
+  const io = IO(server)
 
-    io.on('connect', socket => {
-      const color = socket.request._query.color;
-      console.log(`Trainer ${color} just connected`);
+  const trainer = {
+    red: null,
+    blue: null
+  }
 
-      socket.on('disconnect', () => {
-        console.log(`Trainer ${color} just disconnected`)
-      })
-    });
+  io.on("connect", socket => {
+    const player = socket.request._query.color
+    const t = socket.request._query.t
+    console.log(`Trainer ${player} just connected`)
 
-    console.log(
-      `Your app is up and running on http://localhost:${port}. 
-      You can use playground in development mode on http://localhost:${port}/playground`
-    );
+    trainer[player] = t;
+
+    const opponent = player === "blue" ? "red" : "blue"
+
+    io.emit("action", ({
+      type: `${player.toUpperCase()}_LOGIN`,
+      payload: trainer[player]
+    }))
+
+    if (trainer[opponent]) {
+      socket.emit("action", ({
+        type: `${opponent.toUpperCase()}_LOGIN`,
+        payload: trainer[opponent]
+      }))
+    }
+
+    socket.on("attack", (move, fn) => {
+      socket.broadcast.emit("action", ({
+        type: 'OPPONENT_SELECTED_MOVE',
+        payload: move
+      }))
+    })
+
+    socket.on("disconnect", () => {
+
+      io.emit("action", ({
+        type: `${player.toUpperCase()}_LOGOUT`
+      }))
+
+      trainer[player] = null;
+
+      console.log(`Trainer ${player} just disconnected`)
+    })
   })
-  .catch(error => {
-    console.error(error.stack ? error.stack : error);
-  });
+
+  console.log(
+    `Your app is up and running on http://localhost:${port}.
+      You can use playground in development mode on http://localhost:${port}/playground`
+  )
+})
+.catch(error => {
+  console.error(error.stack ? error.stack : error)
+})
